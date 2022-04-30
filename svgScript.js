08698b44bc989d4f79d264629c8c074c1b94053e
jmg1340 <jordi.miserachs@gmail.com>
2022-04-30 22:09:00 +0200

    commit 7

--- svgScript.js
+++ svgScript.js
@@ -18,21 +18,79 @@ $(document).ready(function () {
 	const vermell = s.select("#cercleVermell")
 	console.log("Vermell", vermell)
 
-	const bbG = groc.getBBox()
-	const bbT = taronja.getBBox()
-	const bbV = vermell.getBBox()
+	// const bbG = groc.getBBox()
+	// const bbT = taronja.getBBox()
+	// const bbV = vermell.getBBox()
 
 	// vermell.transform("s0.87")
 	// vermell.animate({transform: `s0.97 t-${bbT.cx},${bbT.cy}`},2000)
+	const posVermell = {
+		posx : parseInt(vermell.attr("cx")),
+		posy : parseInt(vermell.attr("cy")),
+		radi : vermell.attr("r")
+	}
+	
+	const posTaronja = {
+		posx : parseInt(taronja.attr("cx")),
+		posy : parseInt(taronja.attr("cy")),
+		radi : taronja.attr("r")
+	}
+	
+	const posGroc = {
+		posx : parseInt(groc.attr("cx")),
+		posy : parseInt(groc.attr("cy")),
+		radi : groc.attr("r") -1
+	}
+	
 
-	console.log("bbT.cx", parseInt(bbT.cx),"bbT.cy", parseInt(bbT.cy))
+	console.log("posVermell.posx:", posVermell.posx, "posVermell.posy:", posVermell.posy)
+	console.log("posTaronja.posx:", posTaronja.posx, "posTaronja.posy:", posTaronja.posy)
+	console.log("posGroc.posx:", posGroc.posx, "posGroc.posy:", posGroc.posy)
 	console.log("-------")
 	// vermell.drag()
 
+	// vermell.animate({transform: `s0.97 t-${posTaronja.cx},${posTaronja.cy}`},2000)
+	const intercanviPosicioCercles = function () {
+		vermell.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000, function(){
+			vermell.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000, function(){
+				vermell.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000)
+			})
+		})
+		taronja.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000, function(){
+			taronja.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
+				taronja.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000)
+			})
+		})
+		groc.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
+			groc.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000, function(){
+				groc.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000)
+			})
+		})
+		// console.log("posVermell.posx:", parseInt(vermell.attr("cx")), "posVermell.posy:", vermell.attr("cy"))
+	}
 
-	var moveFunc = function (dx, dy, posx, posy) {
-    this.attr( { cx: posx , cy: posy } ); // basic drag, you would want to adjust to take care of where you grab etc.
-		console.log("posx", posx, "posy", posy)
+
+	$("#intercanviPosicioBtn").on("click", function(){
+		intercanviPosicioCercles()
+		interval2 = setInterval(intercanviPosicioCercles, 24000)
+	})
+
+	$("#stopIntercanviPosicioBtn").on("click", function(){
+		clearInterval(interval2)
+	})
+
+
+
+
+
+
+
+	var moveFunc = function (dx, dy) {
+    this.attr( { 
+			cx: posVermell.posx + dx , 
+			cy: posVermell.posy + dy
+		}); // basic drag, you would want to adjust to take care of where you grab etc.
+		// console.log("posx", posx, "posy", posy)
 	};
 
 	vermell.drag( moveFunc,
@@ -77,15 +135,28 @@ $(document).ready(function () {
 		});
 	};
 
-	blink()
-	setInterval(blink, 12000);
+
+	$("#blinkBtn").on("click", function(){
+		blink()
+		interval1 = setInterval(blink, 12000);
+	})
+
+	$("#stopBlinkBtn").on("click", function(){
+		clearInterval(interval1)
+	})
+
+	
 	
 
 
 
 
-
-
+	const pathG = s.select("#pathG")
+	// groc.attr({
+	// 	cx: 183,
+	// 	cy: 79
+	// })
+	// groc.drawAtPath( pathG, 4000)
 	
 
 	
