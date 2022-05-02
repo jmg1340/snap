b3bd36e286e5da99c7ce668fa6f6229f7996d3ec
jmg1340 <jordi.miserachs@gmail.com>
2022-05-02 18:13:17 +0200

    commit 7

--- svgScript.js
+++ svgScript.js
@@ -24,59 +24,211 @@ $(document).ready(function () {
 
 	// vermell.transform("s0.87")
 	// vermell.animate({transform: `s0.97 t-${bbT.cx},${bbT.cy}`},2000)
-	const posVermell = {
-		posx : parseInt(vermell.attr("cx")),
-		posy : parseInt(vermell.attr("cy")),
-		radi : vermell.attr("r")
+	
+	
+	// posicio i radi de cada cercle
+	const prVermell = {
+		cx : parseInt(vermell.attr("cx")),
+		cy : parseInt(vermell.attr("cy")),
+		r : vermell.attr("r")
 	}
 	
-	const posTaronja = {
-		posx : parseInt(taronja.attr("cx")),
-		posy : parseInt(taronja.attr("cy")),
-		radi : taronja.attr("r")
+	const prTaronja = {
+		cx : parseInt(taronja.attr("cx")),
+		cy : parseInt(taronja.attr("cy")),
+		r : taronja.attr("r")
 	}
 	
-	const posGroc = {
-		posx : parseInt(groc.attr("cx")),
-		posy : parseInt(groc.attr("cy")),
-		radi : groc.attr("r") -1
+	const prGroc = {
+		cx : parseInt(groc.attr("cx")),
+		cy : parseInt(groc.attr("cy")),
+		r : groc.attr("r") -1
 	}
 	
 
-	console.log("posVermell.posx:", posVermell.posx, "posVermell.posy:", posVermell.posy)
-	console.log("posTaronja.posx:", posTaronja.posx, "posTaronja.posy:", posTaronja.posy)
-	console.log("posGroc.posx:", posGroc.posx, "posGroc.posy:", posGroc.posy)
-	console.log("-------")
+	// console.log("posVermell.posx:", posVermell.posx, "posVermell.posy:", posVermell.posy)
+	// console.log("posTaronja.posx:", posTaronja.posx, "posTaronja.posy:", posTaronja.posy)
+	// console.log("posGroc.posx:", posGroc.posx, "posGroc.posy:", posGroc.posy)
+	// console.log("-------")
+
+
 	// vermell.drag()
 
 	// vermell.animate({transform: `s0.97 t-${posTaronja.cx},${posTaronja.cy}`},2000)
+
+
+	const tempsInOutColom = 4000
+	const tempsTransicio = 6000
+
 	const intercanviPosicioCercles = function () {
-		vermell.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000, function(){
-			vermell.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000, function(){
-				vermell.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000)
+
+		/* ------- CERCLE VERMELL ------- */
+		
+		// desaparicio colom
+		colom.animate({opacity: 0}, tempsInOutColom, function(){
+			// 1er canvi de posicio
+			vermell.animate(prTaronja, tempsTransicio, function(){
+				// aparicio colom 
+				colom.animate({opacity: 1}, tempsInOutColom, function(){
+					// esperem 2 segons
+					colom.animate({}, 2000, function() {
+
+						// desaparicio colom
+						colom.animate({opacity: 0}, tempsInOutColom, function() {
+							// 2onr canvi de posicio
+							vermell.animate(prGroc, tempsTransicio, function(){
+								// aparcio colom
+								colom.animate({opacity: 1}, tempsInOutColom, function(){
+									// esperem 2 segons
+									colom.animate({}, 2000, function() {
+										
+										// desaparicio colom
+										colom.animate({opacity: 0}, tempsInOutColom, function() {
+											// 3er canvi de posicio
+											vermell.animate(prVermell, tempsTransicio, function() {
+												// aparcio colom
+												colom.animate({opacity: 1}, tempsInOutColom, function(){
+													// esperem 2 segons
+													colom.animate({}, 2000,)
+												})
+											})
+										})
+									})
+								})
+							})
+						})
+					})
+				})
 			})
 		})
-		taronja.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000, function(){
-			taronja.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
-				taronja.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000)
+
+
+
+
+		/* ------- CERCLE TARONJA ------- */
+
+		// espera desaparicio colom
+		colom.animate({}, tempsInOutColom, function(){
+			// 1er canvi de posicio
+			taronja.animate(prGroc, tempsTransicio, function(){
+				// espera aparicio colom 
+				colom.animate({}, tempsInOutColom, function(){
+					// esperem 2 segons
+					colom.animate({}, 2000, function() {
+
+						// espera desaparicio colom
+						colom.animate({}, tempsInOutColom, function() {
+							// 2onr canvi de posicio
+							taronja.animate(prVermell, tempsTransicio, function(){
+								// espera aparcio colom
+								colom.animate({}, tempsInOutColom, function(){
+									// esperem 2 segons
+									colom.animate({}, 2000, function() {
+										
+										// espera desaparicio colom
+										colom.animate({}, tempsInOutColom, function() {
+											// 3er canvi de posicio
+											taronja.animate(prTaronja, tempsTransicio, function() {
+												// espera aparcio colom
+												colom.animate({}, tempsInOutColom, function(){
+													// esperem 2 segons
+													colom.animate({}, 2000,)
+												})
+											})
+										})
+									})
+								})
+							})
+						})
+					})
+				})
 			})
 		})
-		groc.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
-			groc.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000, function(){
-				groc.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000)
+
+		/* ------- CERCLE GROC ------- */
+
+		// espera desaparicio colom
+		colom.animate({}, tempsInOutColom, function(){
+			// 1er canvi de posicio
+			groc.animate(prVermell, tempsTransicio, function(){
+				// espera aparicio colom 
+				colom.animate({}, tempsInOutColom, function(){
+					// esperem 2 segons
+					colom.animate({}, 2000, function() {
+
+						// espera desaparicio colom
+						colom.animate({}, tempsInOutColom, function() {
+							// 2onr canvi de posicio
+							groc.animate(prTaronja, tempsTransicio, function(){
+								// espera aparcio colom
+								colom.animate({}, tempsInOutColom, function(){
+									// esperem 2 segons
+									colom.animate({}, 2000, function() {
+										
+										// espera desaparicio colom
+										colom.animate({}, tempsInOutColom, function() {
+											// 3er canvi de posicio
+											groc.animate(prGroc, tempsTransicio, function() {
+												// espera aparcio colom
+												colom.animate({}, tempsInOutColom, function(){
+													// esperem 2 segons
+													colom.animate({}, 2000,)
+												})
+											})
+										})
+									})
+								})
+							})
+						})
+					})
+				})
 			})
 		})
-		// console.log("posVermell.posx:", parseInt(vermell.attr("cx")), "posVermell.posy:", vermell.attr("cy"))
+
+
+		// taronja.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000, function(){
+		// 	taronja.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
+		// 		taronja.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000)
+		// 	})
+		// })
+		// groc.animate({r: posVermell.radi, cx: posVermell.posx, cy: posVermell.posy},8000, function(){
+		// 	groc.animate({r: posTaronja.radi, cx: posTaronja.posx, cy: posTaronja.posy},8000, function(){
+		// 		groc.animate({r: posGroc.radi, cx: posGroc.posx, cy: posGroc.posy},8000)
+		// 	})
+		// })
 	}
 
 
 	$("#intercanviPosicioBtn").on("click", function(){
 		intercanviPosicioCercles()
-		interval2 = setInterval(intercanviPosicioCercles, 24000)
+		// interval2 = setInterval(intercanviPosicioCercles, 24000)
 	})
 
 	$("#stopIntercanviPosicioBtn").on("click", function(){
 		clearInterval(interval2)
+	})
+
+
+
+
+	function blink(){
+		colom.animate({opacity: 0}, 4000, function(){
+			colom.animate({}, 2000, function() {
+				colom.animate({opacity: 1}, 4000, function() {
+					colom.animate({}, 2000)
+				});
+			});
+		});
+	};
+
+
+	$("#blinkBtn").on("click", function(){
+		blink()
+		interval1 = setInterval(blink, 12000);
+	})
+
+	$("#stopBlinkBtn").on("click", function(){
+		clearInterval(interval1)
 	})
 
 
@@ -87,8 +239,8 @@ $(document).ready(function () {
 
 	var moveFunc = function (dx, dy) {
     this.attr( { 
-			cx: posVermell.posx + dx , 
-			cy: posVermell.posy + dy
+			cx: vermell.attr("cx") + dx , 
+			cy: vermell.attr("cy") + dy
 		}); // basic drag, you would want to adjust to take care of where you grab etc.
 		// console.log("posx", posx, "posy", posy)
 	};
@@ -125,33 +277,13 @@ $(document).ready(function () {
 	// grupCercles.animate({ transform: "r180," + bbox.cx + ',' + bbox.cy }, 2000);
 
 
-	function blink(){
-		colom.animate({opacity: 0}, 4000, function(){
-			colom.animate({}, 2000, function() {
-				colom.animate({opacity: 1}, 4000, function() {
-					colom.animate({}, 2000)
-				});
-			});
-		});
-	};
-
-
-	$("#blinkBtn").on("click", function(){
-		blink()
-		interval1 = setInterval(blink, 12000);
-	})
-
-	$("#stopBlinkBtn").on("click", function(){
-		clearInterval(interval1)
-	})
-
 	
 	
 
 
 
 
-	const pathG = s.select("#pathG")
+	// const pathG = s.select("#pathG")
 	// groc.attr({
 	// 	cx: 183,
 	// 	cy: 79
